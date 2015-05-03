97b5c650918dcbc8872ee17c72dd98b86656053f
Christoph Miksche <wronnay@gmail.com>
2015-05-03 15:22:32 +0200

    Code update

--- install.sh
+++ install.sh
@@ -27,9 +27,9 @@ service fail2ban restart
 # Daily Update
 su -c "echo -e \"#! /bin/sh\napt-get update && apt-get upgrade -y\" >> /etc/cron.daily/update && chmod a+x /etc/cron.daily/update"
 # Change SSH Port
-sed -i 's/Port 22/Port ${sshport}/g' /etc/ssh/sshd_config
+sed -i "s/Port 22/Port $sshport/g" /etc/ssh/sshd_config
 # Allow Ports
-ufw allow proto tcp from any to any port $sshport
+ufw allow proto tcp from any to any port "$sshport"
 ufw allow http
 ufw allow https
 ufw allow ftp
@@ -37,20 +37,25 @@ ufw allow ftp
 ufw enable
 
 # Change E-Mail in rkhunter config
-sed -i 's/user@domain.tld/${systemmail}/g' /etc/rkhunter.conf
+sed -i "s/user@domain.tld/$systemmail/g" /etc/rkhunter.conf
+sed -i "s/me@mydomain/$systemmail/g" /etc/rkhunter.conf
+sed -i "s/root@mydomain//g" /etc/rkhunter.conf
+sed -i "s/#MAIL-ON-WARNING/MAIL-ON-WARNING/g" /etc/rkhunter.conf
+sed -i "s/#MAIL_CMD/MAIL_CMD/g" /etc/rkhunter.conf
 
 # Add Default User
-useradd -m -c "Default User" $defaultuser
+useradd -m -c "Default User" "$defaultuser"
 # Set Passwort for Default User
-echo $defaultpass | passwd $defaultuser --stdin
+echo "$defaultpass" | passwd "$defaultuser" --stdin
 addgroup --system sshusers
-adduser $defaultuser sshusers
+adduser "$defaultuser" sshusers
 # Set SSH-Users
-sed -i 's/AllowUsers/AllowUsers ${defaultuser}/g' /etc/ssh/sshd_config
+sed -i "s/AllowUsers/AllowUsers $defaultuser/g" /etc/ssh/sshd_config
 # Set SSH-Groups
-sed -i 's/AllowGroups/AllowGroups sshusers/g' /etc/ssh/sshd_config
+sed -i "s/AllowGroups/AllowGroups sshusers/g" /etc/ssh/sshd_config
 # Disable SSH-Root-Login
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+sed -i "s/PermitRootLogin without-password/PermitRootLogin no/g" /etc/ssh/sshd_config
 
 # Copy Shell-Login-File
 cp ./copy/shell-login.sh /opt/shell-login.sh
